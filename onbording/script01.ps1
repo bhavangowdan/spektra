83bc597a7f1ae870d163dd31aa82def7ab92241c
bhavangowdan <83076341+bhavangowdan@users.noreply.github.com>
2024-01-24 21:32:34 +0530

    Update script01.ps1

--- onbording/script01.ps1
+++ onbording/script01.ps1
@@ -48,7 +48,7 @@ InstallAzPowerShellModule
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-Install-Module -Name Microsoft.Identity.Client -Force
+Install-Module -Name Az -AllowClobber -Scope CurrentUser -Force
 
 
 
