f0b8e0a2c5b1e2db3c65294002ff685244685c78
bhavangowdan <83076341+bhavangowdan@users.noreply.github.com>
2024-01-24 21:35:44 +0530

    Create logontask.ps1

---
+++ onbording/logontask.ps1
@@ -0,0 +1,27 @@
+Start-Transcript -Path C:\WindowsAzure\Logs\logontask.txt -Append
+
+
+
+. C:\LabFiles\AzureCreds.ps1
+
+$userName = $AzureUserName
+$password = $AzurePassword
+$deploymentID = $DeploymentID
+
+$securePassword = $password | ConvertTo-SecureString -AsPlainText -Force
+$cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $userName, $SecurePassword
+
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
+
+
+Connect-AzAccount -Credential $cred | Out-Null
+
+
+$rgName = "entp-02-" + $deploymentID
+$vmName = "labvm-" + $deploymentID
+
+Get-AzRemoteDesktopFile -ResourceGroupName "$rgName" -Name "$vmName" -LocalPath "C:\Users\Udacity-Student\Desktop\labvm.rdp"
+
+sleep 10
+
+Unregister-ScheduledTask -TaskName "startextension" -Confirm:$false
