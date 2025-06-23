2ba2c2afaec8bc6ef766dc2071bec619e1a03f40
bhavangowdan <83076341+bhavangowdan@users.noreply.github.com>
2025-06-23 16:16:56 +0530

    Update psscript001.ps1

--- az140/psscript001.ps1
+++ az140/psscript001.ps1
@@ -43,9 +43,34 @@ $commonscriptpath = "$path" + "\cloudlabs-common\cloudlabs-windows-functions.ps1
 
 # Run Imported functions from cloudlabs-windows-functions.ps1
 CloudlabsManualAgent Install
-WindowsServerCommon
+[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls" 
+Disable-InternetExplorerESC
+Enable-IEFileDownload
+Enable-CopyPageContent-In-InternetExplorer
+InstallChocolatey
+DisableServerMgrNetworkPopup
+DisableWindowsFirewall
 InstallCloudLabsShadow $ODLID $InstallCloudLabsShadow
 Enable-CloudLabsEmbeddedShadow $vmAdminUsername $trainerUserName $trainerUserPassword
+
+# Enable Edge
+$shortcutName = "EDGE.lnk"
+$targetPath = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+$desktopPath = [Environment]::GetFolderPath("Desktop")
+$shortcutPath = Join-Path $desktopPath $shortcutName
+
+# Create WScript.Shell COM object
+$WScriptShell = New-Object -ComObject WScript.Shell
+
+# Create the shortcut
+$Shortcut = $WScriptShell.CreateShortcut($shortcutPath)
+$Shortcut.TargetPath = $targetPath
+$Shortcut.WorkingDirectory = Split-Path $targetPath
+$Shortcut.IconLocation = "$targetPath,0"
+$Shortcut.Save()
+
+
 
 # Excel Short cut
 $shortcutName = "EXCEL.lnk"
