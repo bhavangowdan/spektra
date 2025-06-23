8d26e65c6b4b79cf53df5781e4bb3c568e0e3c87
bhavangowdan <83076341+bhavangowdan@users.noreply.github.com>
2025-06-23 11:50:59 +0530

    Create psscript001.ps1

---
+++ az140/psscript001.ps1
@@ -0,0 +1,51 @@
+Param (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $AzureUserName,
+
+    [string]
+    $AzurePassword,
+
+    [string]
+    $AzureTenantID,
+
+    [string]
+    $AzureSubscriptionID,
+
+    [string]
+    $ODLID,
+    
+    [string]
+    $DeploymentID,
+
+    [string]
+    $InstallCloudLabsShadow,
+    
+    [string]
+    $vmAdminUsername,
+
+    [string]
+    $trainerUserName,
+
+    [string]
+    $trainerUserPassword
+)
+
+Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt -Append
+[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls" 
+
+#Import Common Functions
+$path = pwd
+$path=$path.Path
+$commonscriptpath = "$path" + "\cloudlabs-common\cloudlabs-windows-functions.ps1"
+. $commonscriptpath
+
+# Run Imported functions from cloudlabs-windows-functions.ps1
+CloudlabsManualAgent Install
+WindowsServerCommon
+InstallCloudLabsShadow $ODLID $InstallCloudLabsShadow
+CreateCredFile $AzureUserName $AzurePassword $AzureTenantID $AzureSubscriptionID $DeploymentID $ODLID
+Enable-CloudLabsEmbeddedShadow $vmAdminUsername $trainerUserName $trainerUserPassword
+choco install excel
+choco install powerautomatedesktop -y
